725bdd0a5ad669e876bc56810a58336ffecbda08
amerihn <amerihn.art@gmail.com>
2024-07-15 22:59:23 +0700

    added hero

---
+++ sideBar.js
@@ -0,0 +1,8 @@
+function handleClick() {
+  var x = document.getElementById("navbarid");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
