a51c2aa132521c51e275f25749a9e797f23b55f0
amerihn <amerihn.art@gmail.com>
2024-07-19 22:21:02 +0700

    update

--- medical_cra/src/Components/Navbar/Nav.js
+++ medical_cra/src/Components/Navbar/Nav.js
@@ -12,35 +12,43 @@ function Navbar() {
           <i className="fa fa-user-md" aria-hidden="true"></i>
         </a>
       </div>
-      <input type="checkbox" id="check"></input>
+      <input type="checkbox" id="check"/>
       <label for="check" className="side_menu">
         <i className="fa fa-times fa fa-bars" id="menu-icon"></i>
         <i className="fa fa-times" aria-hidden="true" id="close-icon"></i>
       </label> 
       <nav className="navbar">
         <ul>
-          <li className="link"><a href="#">Home</a></li>
-          <li className="link"><a href="#">Appointments</a></li>
-          <li className="link"><a href="#">Health Blog</a></li>
-          <li className="link"><a href="#">Review</a></li>
+          <li className="link">
+            <a href="#">Home</a>
+          </li>
+          <li className="link">
+            <a href="#">Appointments</a>
+          </li>
+          <li className="link">
+            <a href="#">Health Blog</a>
+          </li>
+          <li className="link">
+            <a href="#">Review</a>
+          </li>
           <li className=" link dropdown" id="profile">
             <a className="dropbtn">Profile</a>
             <ul className="dropdown-menu" id="dropdown-pf">
-              <li><a href="#">Reports</a></li>
-              <li><a href="#">Settings</a></li>
+              <li>
+                <a href="#">Reports</a>
+              </li>
+              <li>
+                <a href="#">Settings</a>
+              </li>
             </ul>
           </li>
         </ul>
-        <div>
-          <button className="btn-log">
-            <Link to="/signup">
-              Sign Up
-            </Link>
+        <div className="btn-log">
+          <button>
+            <Link to="/signup">Sign Up</Link>
           </button>
-          <button className="btn-log">
-            <Link to="/login">
-              Login
-            </Link>
+          <button>
+            <Link to="/login">Login</Link>
           </button>
         </div>
       </nav>
